c67d1c01699c17de5bc9ebea2f21f3c9e26e2949
Oleksandr Ponomarov <45709081+aleksandr-ponomarovv@users.noreply.github.com>
2022-10-18 16:03:04 +0300

    refactoring, switched mind map screen from xib to programmatically

--- MindMap/Source/MindMap/MindMapViewController.swift
+++ MindMap/Source/MindMap/MindMapViewController.swift
@@ -7,7 +7,7 @@ protocol MindMapViewType: AnyObject {
 
 class MindMapViewController: UIViewController {
     
-    private var mapScrollView = MapScrollView()
+    private let mapScrollView = MapScrollView()
     
     var presenter: MindMapPresenterType?
     
@@ -45,19 +45,41 @@ extension MindMapViewController: MapScrollViewDelegate {
 // MARK: - Private methods
 private extension MindMapViewController {
     func configureUI() {
-        guard let presenter = presenter else { return }
-        
-        view.addSubview(mapScrollView)
+        view.backgroundColor = .white
         setupMapScrollView()
-        mapScrollView.configureUI(mapFile: presenter.mapFile)
-        mapScrollView.mapDelegate = self
     }
     
     func setupMapScrollView() {
-        mapScrollView.translatesAutoresizingMaskIntoConstraints = false
-        mapScrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        mapScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        mapScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        mapScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        guard let presenter = presenter else { return }
+        mapScrollView.configureUI(mapFile: presenter.mapFile)
+        mapScrollView.mapDelegate = self
+        
+        view.addSubview(mapScrollView)
+        mapScrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
+
+#if targetEnvironment(simulator)
+import SwiftUI
+
+@available(iOS 15, *)
+struct MindMapViewController_Preview: PreviewProvider {
+    
+    static var previews: some View {
+        Group {
+            UIKitControllerPreview {
+                let viewController = MindMapViewController()
+                let node = Node(name: "NodeName", centerPosition: .init())
+                node.add(child: Node(name: "Child", centerPosition: .init(x: 100, y: 200)))
+                let configurator =
+                MindMapConfigurator(mapFile: MapFile(rootNode: node,
+                                                     contentViewSize: .init()))
+                configurator.configure(viewController: viewController)
+                return viewController
+            }
+        }
+    }
+}
+#endif
